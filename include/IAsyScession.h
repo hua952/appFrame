26fdbd6db5d5fce7f9b3a8ff7ec1e22b0ae3f22b
hua952 <hua_0_@hotmail.com>
2021-06-19 23:09:53 +0800

    OK

--- include/IAsyScession.h
+++ include/IAsyScession.h
@@ -5,9 +5,23 @@
 #include "packet.h"
 enum IAsyScessionState
 {
+	oK_IAsyScessionState,
+	disCon_IAsyScessionState,
+	closed_IAsyScessionState
+};
+
+struct IAsyScessionCallBack
+{
+	virtual onStateChangea(IAsyScessionState from, IAsyScessionState to) = 0;
+	virtual onRecPack(packetHead* pack) = 0;
 };
 struct IAsyScession
 {
+	virtual void* userData() = 0;
+	virtual void  setUserData(void* pData) = 0;
+	virtual IAsyScessionState state() =0;
 	virtual int send(packetHead* pack) = 0;
+	virtual int close() = 0;
 };
+
 #endif 
